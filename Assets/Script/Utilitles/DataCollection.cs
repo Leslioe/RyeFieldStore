d03b36b88ba98348edfbbf9922a962f96e47ff80
Leslioe <1642361892@qq.com>
2023-10-29 18:07:03 +0800

    十月二十九日

---
+++ Assets/Script/Utilitles/DataCollection.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[System.Serializable]
+public class ItemDetails
+{
+    public int itemID;
+    public string itemName;
+    public ItemType itemType;
+    public Sprite itemIcon;
+    public Sprite itemOnWorldSprite;
+    public string itemDescription;
+    public int itemUseRadius;
+    public bool canPickedup;
+    public bool canDropped;
+    public bool canCarried;
+    public int itemPrice;
+    [Range(0, 1)]
+    public float sellPercentage;
+}
